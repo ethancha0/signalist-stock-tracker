d94c3380ccdba8e7eefbd85ddf301c2d9b5523b7
ethancha0 <ethanchao2005@gmail.com>
2026-02-10 15:46:12 -0800

    feat: refactored trading view widget component for reusability

---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import useTradingViewWidget from '@/hooks/useTradingViewWidget';
+import React, { useEffect, useRef, memo } from 'react';
+import {cn} from "@/lib/utils";
+
+interface TradingViewWidgetProps{
+    title?: string;
+    scriptUrl: string;
+    config: Record<string, unknown>;
+    height?: number;
+    className?: string;
+}
+
+
+
+function TradingViewWidget({ title, scriptUrl, config, height = 600, className }: TradingViewWidgetProps) {
+  const container = useTradingViewWidget(scriptUrl, config, height);
+
+
+  return (
+
+
+    <div className="w-full">
+        {title && <h3 className="font-semibold text-xl text-gray-100 mb-5">{title}</h3>}
+        <div className={cn('tradingview-widget-container', className)}  ref={container} style={{ height: "100%", width: "100%" }}>
+            <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }}/>
+            </div>
+    </div>
+    
+  );
+}
+
+export default memo(TradingViewWidget);
